e8ce67a92579caa1661865fc907bb00cee948d41
kraktos <arnab.dutta@gmail.com>
2014-08-08 12:05:07 +0200

    Base Version

---
+++ src/main/java/reasoning/TemporalReasoning/App.java
@@ -0,0 +1,13 @@
+package reasoning.TemporalReasoning;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
